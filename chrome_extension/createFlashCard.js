b9d27f6a49fee9d87e23c2f4a088c0d8cd38e348
Julian <yang.julian@gmail.com>
2018-12-31 17:30:38 -0800

    Test making ajax call to different http address.

--- chrome_extension/createFlashCard.js
+++ chrome_extension/createFlashCard.js
@@ -9,20 +9,7 @@ chrome.runtime.onMessage.addListener(request => {
     if (!traditionalCedict) { 
       loadCedict();
     }
-    // document.body.innerHTML +=`<dialog style="height:40%"> <iframe id="flashCardCreator"style="height:100%"></iframe>
-    //         <div style="position:absolute; top:0px; left:5px;">  
-    //             <button>x</button>
-    //         </div>
-    //         </dialog>`;
-    // const dialog = document.querySelector("dialog");
-    // dialog.showModal();
-    // const iframe = document.getElementById("flashCardCreator");
-    // iframe.srcdoc = "<p>Hello world!</p> <script src=\"" + chrome.extension.getURL("loadAngularDart.js") +  "\"></script>";
-    // // iframe.src = chrome.extension.getURL("index.html");
-    // iframe.frameBorder = 0;
-    // dialog.querySelector("button").addEventListener("click", () => {
-    //     dialog.close();
-    // });
+
     document.body.innerHTML += `
       <div id="cta-dialog" title="Create new Anki flashcard">
         
@@ -32,6 +19,7 @@ chrome.runtime.onMessage.addListener(request => {
       </div>
     `;
 
+    // setup JQuery stuff.
     $( function() {
       chrome.storage.sync.get(['selectionInfo'], function(result) {
         console.log('Value of selectionInfo is ' + JSON.stringify(result));
@@ -46,10 +34,20 @@ chrome.runtime.onMessage.addListener(request => {
       });
 
       $("#cta-dialog").dialog();
+
+      // Test fetching from other domains.
+      // $.ajax({
+      //   url: 'https://www.github.com',
+      //   type: 'GET',
+      //   dataType: 'html',
+      //   success: function(data) {
+      //     console.log(data);
+      //   },
+      //   error: function(request, error) {
+      //     alert('Could not load Cedict!\nRequest: '+JSON.stringify(request)+'\nerror: ' + JSON.stringify(error));
+      //   }
+      // });
     });
-
-
-
   }
 });
 
