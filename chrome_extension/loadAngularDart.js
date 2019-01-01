53b064fd014f43af0f4fc9d112eab157db186652
Julian <yang.julian@gmail.com>
2018-12-31 17:29:19 -0800

    Cleanup repo structure: move old scripts to old_script/ directory.

--- chrome_extension/loadAngularDart.js
+++
@@ -1,5 +0,0 @@
-// chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.get(['selectionInfo'], function(result) {
-        console.log('Value of selectionInfo is ' + result);
-    })
-// })
