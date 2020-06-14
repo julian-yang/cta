5694edb0fff6a71c9bcd682402412ae559e9a8d1
Julian Yang <yang.julian@gmail.com>
2020-06-14 00:37:09 -0700

    Update chip some more

--- chinesetextloader/lib/article_table.dart
+++ chinesetextloader/lib/article_table.dart
@@ -126,26 +126,9 @@ class _ArticleTableState extends State<ArticleTable> {
         HeaderDragTarget(index),
         Draggable<DataColumnConfig>(
             child: headerChip,
-            feedback: Material(
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(config.name, style: TextStyle(fontSize: 16)),
-                  )),
-            ),
-            childWhenDragging: Opacity(
-              opacity: .5,
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.rectangle,
-                  ),
-                  child: Text('childWhenDragging')),
-            ),
+            feedback: FakeChip(config.name),
+            childWhenDragging:
+                Opacity(opacity: .5, child: FakeChip(config.name)),
             data: config),
       ],
     );
@@ -196,4 +179,28 @@ class _ArticleTableState extends State<ArticleTable> {
         width: 120,
         sortAscending: true),
   ];
+}
+
+class FakeChip extends StatelessWidget {
+  final String _text;
+
+  const FakeChip(
+    this._text, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: DecoratedBox(
+          decoration: BoxDecoration(
+              color: Colors.grey[300],
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(16.0))),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(_text, style: TextStyle(fontSize: 16)),
+          )),
+    );
+  }
 }
