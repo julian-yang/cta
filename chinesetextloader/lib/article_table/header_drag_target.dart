ed75d1849d2a830822a3d04a9840209fa4d955eb
Julian Yang <yang.julian@gmail.com>
2020-06-14 14:44:57 -0700

    Hide targets until dragging

--- chinesetextloader/lib/article_table/header_drag_target.dart
+++ chinesetextloader/lib/article_table/header_drag_target.dart
@@ -4,6 +4,7 @@ import '../article_wrapper.dart';
 import '../utils.dart';
 import 'package:provider/provider.dart';
 import 'column_config.dart';
+import 'drag_state_model.dart';
 
 class HeaderDragTarget extends StatefulWidget {
   final int index;
@@ -17,23 +18,31 @@ class HeaderDragTarget extends StatefulWidget {
 class _HeaderDragTargetState extends State<HeaderDragTarget> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ColumnConfigModel>(
-        builder: (context, configModel, child) => DragTarget<ColumnConfig>(
-            builder: (context, candidates, rejects) => DecoratedBox(
-                decoration: BoxDecoration(
-                  color: candidates.isNotEmpty ? Colors.red : Colors.orange,
-                  shape: BoxShape.rectangle,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text('target'),
-                )),
-            onWillAccept: (data) =>
-                data != configModel.columns[widget.index]
-                ,
-            onAccept: (data) {
-              configModel.rearrange(data, widget.index);
-//              showDialog(context: context, child: Text('Accepted!'));
-            }));
+    return Consumer2<ColumnConfigModel, DragStateModel>(
+        builder: (context, configModel, dragStateModel, child) =>
+            DragTarget<ColumnConfig>(
+                builder: (context, candidates, rejects) =>
+                    dragStateModel.draggedColumn != null
+                        ? buildTargetBox(candidates)
+                        : SizedBox.shrink(),
+                onWillAccept: (data) {
+                  int index = configModel.columns.indexOf(data);
+                  return index != widget.index && index != widget.index - 1;
+                },
+                onAccept: (data) {
+                  configModel.rearrange(data, widget.index);
+                }));
+  }
+
+  Widget buildTargetBox(List<ColumnConfig> candidates) {
+    return DecoratedBox(
+        decoration: BoxDecoration(
+          color: candidates.isNotEmpty ? Colors.red : Colors.orange,
+          shape: BoxShape.rectangle,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text('target'),
+        ));
   }
 }
